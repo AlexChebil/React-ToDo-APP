e4ec90055d876d940ad83d866c0df95425324a92
Skander <skanderchebil1@gmail.com>
2021-12-26 01:42:24 +0100

    still stuck on edit func but ive made a good leap

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,28 @@ function App() {
   let RefName = useRef();
   const RefAge = useRef();
 
+  let EditRefName = useRef();
+  const EditRefAge = useRef();
+
   const [agents, updateAgents] = useState([]);
   const [required, setRequired] = useState(false); //for the warning on blank submit
+  const [editToggle, setEditToggle] = useState(false);
+  const [editName, setEditName] = useState("");
+  const [editAge, setEditAge] = useState(Number); //set to number to deal with a warning in console
 
-  const uuid = uuidv4();
+  const uuid = uuidv4(); // for the agents {id}
   const agentsNumber = agents.length;
 
   function addAgents(e) {
     e.preventDefault();
-    let nameValue = RefName.current.value;
+
+    let nameValue = RefName.current.value; // the current value for the name input
     let ageValue = RefAge.current.value;
 
-    if (!ageValue) {
+    if (!ageValue || !nameValue) {
+      //   || = Logical OR
       setRequired(true);
-      return;
+      return; // to defend user from passing data without all inputs being filled
     }
 
     updateAgents((prev) => {
@@ -32,42 +40,87 @@ function App() {
   }
 
   useEffect(() => {
-    const saved = JSON.parse(localStorage.getItem("data"));
+    const saved = JSON.parse(localStorage.getItem("data")); //get our saved data
     saved && updateAgents(saved);
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("data", JSON.stringify(agents));
+    localStorage.setItem("data", JSON.stringify(agents)); //save the input data
   }, [agents]);
 
   function Delete(prop) {
-    let temporaryList = [...agents];
-    const found = temporaryList.findIndex((entry) => entry.id === prop);
-    temporaryList.splice(found, 1);
-    updateAgents(temporaryList);
+    let temporaryList = [...agents]; // we do not directly work with the main array this is a clone
+    const remove = temporaryList.filter((entry) => entry.id !== prop);
+    updateAgents(remove);
   }
 
   function Edit(prop) {
-    let temporaryList = [...agents];
-    const found = temporaryList.find((entry) => entry.id === prop);
-    console.log(found.name);
+    let agentsClone = agents;
+    agentsClone.map((entry) =>
+      entry.id === prop ? setEditName(entry.name) + setEditAge(entry.age) : null
+    );
+  }
+
+  function editSubmitButton(prop) {
+    let agentsClone2 = agents;
+    let foundIndex = agentsClone2.findIndex((entry) => entry.id === prop);
+    let foundElem = agentsClone2.find((entry) => entry.id === prop);
+    let EditRefNameValue = EditRefName.current.value; // the current value for the name input
+    let EditRefAgeValue = EditRefAge.current.value;
+    updateAgents((prev) => {
+      return [
+        ...prev,
+        { name: EditRefNameValue[foundIndex], age: EditRefAgeValue, id: uuid },
+      ];
+    });
   }
 
   return (
-    <div className='main'>
+    <div className={`main ${CSS ? "active" : null}`}>
       <h1>CRUD</h1>
       <h3>We Currently Have {agentsNumber} Agents In Active Duty </h3>
-      <Inputs input={agents} Delete={Delete} Edit={Edit} />
+
+      <Inputs
+        input={agents}
+        Delete={Delete}
+        Edit={Edit}
+        editToggle={setEditToggle}
+        editButton={editSubmitButton}
+      />
+
       <form>
         <div>
-          <input type='text' placeholder='Enter Name' ref={RefName} required />
-          <input type='number' placeholder='Enter Age' ref={RefAge} required />
+          <input type='text' placeholder='Enter Name' ref={RefName} />
+          <input type='number' placeholder='Enter Age' ref={RefAge} />
+
           {required && (
             <div className='warings'>Please Fill Out All Inputs</div>
           )}
+
           <button id='addButton' onClick={(e) => addAgents(e)}>
             Submit
           </button>
+        </div>
+
+        <div className={`edit ${editToggle ? "active" : null}`}>
+          <h2>Update Palace</h2>
+          <input
+            type='text'
+            placeholder='Enter New Name'
+            value={editName}
+            onChange={(e) => setEditName(e.target.value)}
+            ref={EditRefName}
+            required
+          />
+          <input
+            type='number'
+            value={editAge}
+            onChange={(e) => setEditAge(e.target.value)}
+            placeholder='Enter New Age'
+            ref={EditRefAge}
+            required
+          />
+          <button onClick={() => editSubmitButton()}>EDIT SUBMIT</button>
         </div>
       </form>
     </div>
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
-function Inputs({ input, Delete, Edit }) {
+function Inputs({ input, Delete, Edit, editToggle, editButton }) {
   function eventHandler(prop) {
     Delete(prop);
   }
   function editHandler(prop) {
     Edit(prop);
+    editToggle((prev) => !prev);
+  }
+
+  function submitHandler(prop) {
+    editButton(prop);
+  }
+  function test(prop) {
+    editHandler(prop);
+    submitHandler(prop);
   }
   return (
     <div className='core'>
       {input.map((entry) => {
         return (
           <div className='child' key={entry.id}>
-            <button onClick={() => editHandler(entry.id)}>Edit</button>
+            <button onClick={() => test(entry.id)}>Edit</button>
             <div>
               <span>{entry.name}</span>
               <span>{entry.age}</span>
